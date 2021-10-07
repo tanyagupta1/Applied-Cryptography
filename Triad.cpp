1eb95e9411b45aa642cd79984c8050eed18bbf43
tanyagupta1 <tanya19119@iiitd.ac.in>
2021-10-08 02:24:27 +0530

    major bug fix

--- Triad.cpp
+++ Triad.cpp
@@ -1,25 +1,38 @@
 #include<bits/stdc++.h>
 using namespace std;
 #define uc unsigned char
-
-vector<uc> triad_encrypt(vector<uc>K, vector<uc>N, vector<uc> M);
-vector<uc> triad_decrypt(vector<uc>K, vector<uc>N, vector<uc> C);
-vector<uc> triad_sc(vector<uc>K, vector<uc>N, vector<uc> M);
+int DONE =20000000;
+vector<bool> triad_encrypt(vector<uc>K, vector<uc>N, vector<bool> M);
+vector<bool> triad_decrypt(vector<uc>K, vector<uc>N, vector<bool> C);
+vector<bool> triad_sc(vector<uc>K, vector<uc>N, vector<bool> M);
 vector<vector<uc>> triadUpd(vector<uc>a, vector<uc> b, vector<uc> c, bool msg);
 vector<vector<uc>> triadP(vector<uc>a,vector<uc>b,vector<uc> c);
 
-vector<uc> triad_encrypt(vector<uc>K, vector<uc>N, vector<uc> M)
+vector<bool> triad_encrypt(vector<uc>K, vector<uc>N, vector<bool> M)
 {
-    vector<uc> C = triad_sc(K,N,M);
+    vector<bool> C = triad_sc(K,N,M);
     return C;
 }
-
-vector<uc> triad_decrypt(vector<uc>K, vector<uc>N, vector<uc> C)
+void print(vector<uc> v)
 {
-    vector<uc> M = triad_sc(K,N,C);
+    int l=v.size();
+    for(int i=0;i<l;i++) cout<<(int)v[i]<<' ';
+    cout<<'\n';
+    DONE--;
+}
+void print2(vector<bool> v)
+{
+    int l=v.size();
+    for(int i=1;i<l;i++) cout<<(int)v[i];
+    cout<<'\n';
+    DONE--;
+}
+vector<bool> triad_decrypt(vector<uc>K, vector<uc>N, vector<bool> C)
+{
+    vector<bool> M = triad_sc(K,N,C);
     return M;
 }
-vector<uc> triad_sc(vector<uc>K, vector<uc>N, vector<uc> M)
+vector<bool> triad_sc(vector<uc>K, vector<uc>N, vector<bool> M)
 {
     vector<uc>con = {0xFE, 0xFF, 0xFF, 0xFF};
     vector<uc>a ={N[0],K[4],con[3],K[3],con[2],K[2],con[1],K[1],con[0],K[0]} ;
@@ -35,8 +48,8 @@ vector<uc> triad_sc(vector<uc>K, vector<uc>N, vector<uc> M)
     b = tmp[1];
     c = tmp[2];
     bool z;
-    int mlen = M.size();
-    vector<uc> C(mlen);
+    int mlen = M.size()/8;
+    vector<bool> C(M.size());
     for(int i=0;i<mlen;i++)
     {
         C[i] = 0;
@@ -47,8 +60,13 @@ vector<uc> triad_sc(vector<uc>K, vector<uc>N, vector<uc> M)
             b = tmp[1];
             c = tmp[2];
             z = tmp[3][0];
-            C[i] += pow(2,j) *(M[i] & (1<<j));
+
+            // print(a); print(b); print(c); 
+            cout<<"z:"<<z<<' ';
+            C[i*8+j] = M[i*8+j]^z;
+
         }
+        cout<<'\n';
     }
     return C;
 }
@@ -62,10 +80,13 @@ vector<vector<uc>>triadP(vector<uc>a,vector<uc>b,vector<uc> c)
         a = tmp[0];
         b = tmp[1];
         c = tmp[2];
+        // print(a); print(b); print(c);
     }
+
     return vector<vector<uc>>{a,b,c};
 
 }
+int ccount=40;
 vector<vector<uc>> triadUpd(vector<uc>a, vector<uc> b, vector<uc> c, bool msg)
 {
     vector<bool>abit(81); vector<bool>bbit(89); vector<bool>cbit(89);
@@ -74,26 +95,30 @@ vector<vector<uc>> triadUpd(vector<uc>a, vector<uc> b, vector<uc> c, bool msg)
     {
         for(int j=0;j<8;j++)
         {
-            abit[i*8+j+1] = a[i] & (1<<(7-j));
+            abit[i*8+j+1] = (a[i] & (1<<(7-j)))>>(7-j);
         }
     }
     for(int i=0;i<11;i++)
     {
         for(int j=0;j<8;j++)
         {
-            bbit[i*8+j+1] = b[i] & (1<<(7-j));
-            cbit[i*8+j+1] = c[i] & (1<<(7-j));
+            bbit[i*8+j+1] = (b[i] & (1<<(7-j)))>>(7-j);
+            cbit[i*8+j+1] = (c[i] & (1<<(7-j)))>>(7-j);
         }
     }
-    
     t1 = abit[68] ^ abit[80] ^ bbit[85]*cbit[85];
     t2 = bbit[64] ^ bbit[88];
     t3 = cbit[68] ^ cbit[88];
     z = t1 ^ t2 ^ t3;
-    t1 = t1 ^ abit[73]*abit[79] ^ b[66] ^ msg;
+    t1 = t1 ^ abit[73]*abit[79] ^ bbit[66] ^ msg;
     t2 = t2 ^ bbit[65]*bbit[87] ^ cbit[84] ^ msg;
     t3 = t3 ^ cbit[77]*cbit[87] ^ abit[74] ^ msg;
-
+    // if(ccount>0) 
+    // {
+    //     print2(abit); print2(bbit); print2(cbit);
+    //     ccount--;
+    // }
+    // cout<<t1<<' '<<t2<<' '<<t3<<' '<<z<<'\n';
     for(int i=80;i>=2;i--)
     {
         abit[i]=abit[i-1];
@@ -131,10 +156,25 @@ vector<vector<uc>> triadUpd(vector<uc>a, vector<uc> b, vector<uc> c, bool msg)
             base*=2;
         }
     }
+    // if(ccount>0) 
+    // {
+    //     print(a); print(b); print(c);
+    //     ccount--;
+    // }
     return vector<vector<uc>>{a,b,c,vector<uc>{z}};
 }
 int main()
 {
-
+    vector<uc> K={2,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
+    vector<uc> N= {2,1,2,3,4,5,6,7,8,9,10,11};
+    vector<bool> M = {1,1,1,1,1,1,1,1};
+    vector<bool> C = triad_encrypt(K,N,M);
+    cout<<C.size()<<'\n';
+    for(int i=0;i<8;i++) cout<<(int)C[i]<<' ';
+    cout<<'\n';
+    ccount=40;
+    vector<bool> dec= triad_decrypt(K,N,C);
+    for(int i=0;i<8;i++) cout<<(int)dec[i]<<' ';
+    cout<<'\n';
 
 }
